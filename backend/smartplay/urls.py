2591588bbbd37386d5c0a42c320c25e31f1a6376
adarsh-varrier <adarshpmanimandiram@gmail.com>
2025-02-03 22:25:39 +0530

    innathee nirthuva

--- backend/smartplay/urls.py
+++ backend/smartplay/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import test_view
 from .views import RegisterView
 from django.contrib.auth.views import LoginView
-from .views import LogoutView, UserDetailsView
+from .views import LogoutView, UserDetailsView,user_settings
 
 urlpatterns = [
     path('api/test/', test_view, name='test_view'),
@@ -12,4 +12,5 @@ urlpatterns = [
     path('api/login/', views.LoginView.as_view(), name='login'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
     path('api/user-details/', UserDetailsView.as_view(), name='user-details'),
+    path('api/settings/', user_settings, name="user-settings"),
 ]
